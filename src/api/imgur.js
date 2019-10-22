a152f279e4319d7635e44222488e4cb3dd02958b
Rus Hughes <russell.hughes@gmail.com>
2019-10-22 21:01:27 +0200

    s5 l120

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
-import qs from 'qs'
+import qs from 'qs';
+import axios from 'axios';
 
 const CLIENT_ID = process.env.VUE_APP_IMGUR_CLIENT_ID;
 const ROOT_URL = 'https://api.imgur.com';
@@ -11,5 +12,12 @@ export default {
       };
       console.log(querystring);
       window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
+  },
+  fetchImages(token) {
+    return axios.get(`${ROOT_URL}/3/account/me/images`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
   }
 };
