7efbba9e82257e335fd09664f72e01c5667d7e18
Rus Hughes <russell.hughes@gmail.com>
2019-10-22 16:54:52 +0200

    s4 l81

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>Image Storage</div>
+  <div class="ui secondary pointing menu">
+    <a class="active item">
+      Image Storage
+    </a>
+    <div class="right menu">
+      <a class="ui item">
+        Gallery
+      </a>
+      <a class="ui item">
+        Upload
+      </a>
+      <a class="ui item">
+        Logout
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
